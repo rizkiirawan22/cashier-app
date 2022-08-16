1e36ce4a0dff84f82bb2d885ffc7017232b561b6
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-08-16 10:44:04 +0700

    add migration

---
+++ database/migrations/2022_08_16_033443_create_purchase_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchaseDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id')->constrained();
+            $table->foreignId('item_id')->constrained();
+            $table->integer('amount');
+            $table->integer('unit_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_details');
+    }
+}
