69adbe20c072d7dea145f7c53c3ae2fec84e69aa
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-08-16 13:12:54 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PurchaseController;
+use App\Models\Item;
+use App\Models\Purchase;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        'item_count' => Item::count(),
+        'purchase_count' => Purchase::count(),
+    ]);
 });
+
+Route::resource('barang', ItemController::class);
+Route::post('pembelian/cart/remove', [PurchaseController::class, 'removeCart'])->name('pembelian.removeCart');
+Route::post('pembelian/cart/tambah', [PurchaseController::class, 'addCart'])->name('pembelian.addCart');
+Route::post('pembelian/detail', [PurchaseController::class, 'detail'])->name('pembelian.detail');
+Route::resource('pembelian', PurchaseController::class);
